b8da3d0853f1e0a443441f54a4ff364ec0a2f4dd
H. Chase Stevens <chase@chasestevens.com>
2017-06-17 23:05:46 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+ 
+setup(
+    name = 'asttools',
+    packages = [],
+    version = '0.0.0',
+    description = 'Tools for AST construction and manipulation',
+    license='MIT',
+    author='H. Chase Stevens',
+    author_email='chase@chasestevens.com',
+    url='https://github.com/hchasestevens/asttools',
+    install_requires=[
+        'astor',
+    ],
+    entry_points={},
+    keywords='ast asts syntax metaprogramming',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ]
+)
